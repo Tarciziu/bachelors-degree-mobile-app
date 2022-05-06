5179944edc9759f2dbc4fb8da0935766ba6baa3f
Tarciziu Gologan <tarciziu.gologan@nexttech.ro>
2022-05-06 21:05:58 +0300

    Initial Commit

---
+++ BachelorsDegreeApp/BachelorsDegreeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BachelorsDegreeAppApp.swift
+//  BachelorsDegreeApp
+//
+//  Created by Tarciziu Gologan on 06.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BachelorsDegreeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
