0f537110b83089430910feba0e43fd5be85c393c
Tarciziu Gologan <tarciziu.gologan@nexttech.ro>
2022-05-06 21:33:58 +0300

    Add colors catalog
    Change contains:
    * Addition of colors catalog
    
    JIRA ticket: N/A
    
    Type: New Feature

---
+++ BachelorsDegreeApp/Utils/ColorsCatalog.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsCatalog.swift
+//  MentalHealth
+//
+//  Created by Tarciziu Gologan on 06.05.2022.
+//
+
+import SwiftUI
+
+enum ColorsCatalog {
+  static let primaryText = Color("PrimaryText")
+  static let secondaryText = Color("SecondaryText")
+  static let accent = Color("Accent")
+  static let listItemBorder = Color("ListItemBorder")
+  static let listItemShadow = Color("ListItemShadow")
+  static let listItemBackground = Color("ListItemBackground")
+  static let listBackground = Color("ListBackground")
+}
